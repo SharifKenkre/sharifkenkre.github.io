9a735d76a8d15b26fcb7396cf3f9b5f6edf4a095
Swetang Naik <sharifkenkre22@gmail.com>
2025-09-25 18:36:18 +0530

    added all files again

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',          // enables static export (creates /out)
-  images: { unoptimized: true }, // required if you use next/image on static export
-  trailingSlash: true,       // optional but helps with GitHub Pages routing
-};
-module.exports = nextConfig;
